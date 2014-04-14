8f4175d34ce50948e08edf425271761f00fe02bb
sfeller <sarah.feller@twcable.com>
2014-04-13 22:53:48 -0600

    updated file with CPU data

--- data-getter.rb
+++ data-getter.rb
@@ -6,11 +6,10 @@ require 'csv'
 inputFile = "originalData.csv"
 outputFile = "dataWithTemp.csv"
 
-headerIn = ["Date","Value1","Value2"]
+headerIn = ["Date","CPU"]
 
 datapoints = []
 csv = CSV.parse(File.read(inputFile).force_encoding("ISO-8859-1").encode("UTF-8"))
-csv.shift
 csv.each do |data|
    row = Hash[headerIn.zip data]
    row.each {|i, j| row[i] = j }
@@ -18,18 +17,19 @@ csv.each do |data|
 end
 
 output = File.open(outputFile, "w")
-output << "Date,Temperature,Humidity,Value 1,Value 2\n"
+output << "Date,Temperature,Humidity,CPU\n"
 
 #result = File.open("result.txt")
 
 datapoints.each do |data|
 
 	#/(?<month>\d+)\/(?<day>\d+)\/(?<year>\d+)/.match(data["Date"])	
-	date = data["Date"].match(/(\d+)-(\d+)-(\d+)/)
-   `curl -s -O http://www.wunderground.com/history/airport/KBDU/#{date[1]}/#{date[2]}/#{date[3]}/DailyHistory.html?req_city=Boulder&req_state=CO; tr -d "\n" < DailyHistory.html?req_city=Boulder | tee results.txt`
+	date = data["Date"].match(/(\d{4})(\d{2})(\d{2})/)
+   `curl -s -O http://www.wunderground.com/history/airport/KBDU/#{date[1]}/#{date[2]}/#{date[3]}/DailyHistory.html?req_city=Boulder&req_state=CO`
+	`tr -d "\n" < DailyHistory.html?req_city=Boulder | tee results.txt`
    info = File.read("results.txt").force_encoding("ISO-8859-1").encode("UTF-8")
    weather = info.match(/Mean Temperature<\/span><\/td>\s+<td>\s+<span\s+class="nobr"><span\s+class="b">(\d+).+Average Humidity<\/span><\/td>\s+<td>(\d+)/)
-	output << "#{date[1]}-#{date[2]}-#{date[3]},#{weather[1]},#{weather[2]},#{data["Value1"]},#{data["Value2"]}\n"
+	output << "#{date[1]}-#{date[2]}-#{date[3]},#{weather[1]},#{weather[2]},#{data["CPU"]}\n"
 	#file.close
 
 end
